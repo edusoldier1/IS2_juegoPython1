dd3805f73646ae9316cefbc24de25ca01b735c98
A3404 <A3404@lab.aca>
2019-01-09 18:49:33 -0500

    juego de Python, realizar Random al movimiento automatico

---
+++ game.py
@@ -0,0 +1,41 @@
+import pygame
+
+pygame.init()
+velocidad = [2,2]
+
+screen = pygame.display.set_mode( (800,600) )
+
+#He cargado mi sprite en memoria
+spr_ball = pygame.image.load("assets/soccer.png")
+spr_ball = pygame.transform.scale(spr_ball, (64,64))
+rect_ball = spr_ball.get_rect()
+
+
+#Game Loop
+terminado = False
+while not terminado:
+    #gestionar los eventos del usuario
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminado = True
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                rect_ball = rect_ball.move([10,0])
+            elif event.key == pygame.K_LEFT:
+                rect_ball = rect_ball.move([-10,0])
+            elif event.key == pygame.K_UP:
+                rect_ball = rect_ball.move([0,-10])
+            elif event.key == pygame.K_DOWN:
+                rect_ball = rect_ball.move([0,10])
+    #actualizar los estados del juego
+    """rect_ball = rect_ball.move(velocidad)
+    if rect_ball.y > 536 or rect_ball.y <=0:
+        velocidad[1] = -velocidad[1]
+
+    if rect_ball.x > 736 or rect_ball.x <= 0:
+        velocidad[0] = -velocidad[0]"""
+    #renderizar la interfaz gráfica
+    screen.fill((0,0,0))
+    screen.blit(spr_ball, rect_ball)
+    pygame.display.flip()
