d6e198f44447398d5799e800be2e97b6b2dfb947
Eduardo <edu_7398@hotmail.com>
2019-01-11 12:30:25 -0500

    Velocidad aleatoria comentada, movimiento manual activado

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 pygame.init()
-velocidad = [random.randint(0,5),random.randint(0,5)]
+velocidad = [random.randint(1,5),random.randint(1,5)]
 
 screen = pygame.display.set_mode( (800,600) )
 
@@ -20,7 +20,7 @@ while not terminado:
         if event.type == pygame.QUIT:
             terminado = True
             break
-        """elif event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 rect_ball = rect_ball.move([10,0])
             elif event.key == pygame.K_LEFT:
@@ -28,25 +28,25 @@ while not terminado:
             elif event.key == pygame.K_UP:
                 rect_ball = rect_ball.move([0,-10])
             elif event.key == pygame.K_DOWN:
-                rect_ball = rect_ball.move([0,10])"""
+                rect_ball = rect_ball.move([0,10])
     #actualizar los estados del juego
-    if velocidad[0] >= 0:
-        velocidad[0] = random.randint(0,5)
+    """if velocidad[0] > 0:
+        velocidad[0] = random.randint(1,5)
     else:
         velocidad[0] = random.randint(-5,-1)
-    if velocidad[1] >= 0:
-        velocidad[1] = random.randint(0,5)
+    if velocidad[1] > 0:
+        velocidad[1] = random.randint(1,5)
     else:
         velocidad[1] = random.randint(-5,-1)
     rect_ball = rect_ball.move(velocidad)
     if rect_ball.y > 536:
         velocidad[1] = random.randint(-5,-1)
     if rect_ball.y <= 0:
-        velocidad[1] = random.randint(0,5)
+        velocidad[1] = random.randint(1,5)
     if rect_ball.x > 736:
         velocidad[0] = random.randint(-5,-1)
     if rect_ball.x <= 0:
-        velocidad[0] = random.randint(0,5)
+        velocidad[0] = random.randint(1,5)"""
     #renderizar la interfaz grafica
     screen.fill((0,0,0))
     screen.blit(spr_ball, rect_ball)
